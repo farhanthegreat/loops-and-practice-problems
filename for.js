1df1884dbc34ac9e2e2bfc17c7a522377c735acf
farhanthegreat <farhantuber@gmail.com>
2023-02-01 19:20:22 +0600

    for loop added

---
+++ for.js
@@ -0,0 +1,23 @@
+// var roastGiven = 0;
+// while (roastGiven < 7)
+// {
+//     console.log('please roast den');
+//     roastGiven++;
+// }
+
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++)
+// {
+//     console.log('roast de re vai');
+// }
+
+//simple version
+/* console.log('simple output');
+for(i = 0; i < 10; i++)
+{
+    console.log(i);
+} */
+
+for (var i = 1; i <= 20; i+=2)
+{
+    console.log(i);
+}
