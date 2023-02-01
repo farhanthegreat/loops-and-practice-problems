bb28cc2969a98f02327406ecd4993f23eb56e452
farhanthegreat <farhantuber@gmail.com>
2023-02-01 19:04:36 +0600

    while extra added odd even

---
+++ even.js
@@ -0,0 +1,12 @@
+var number = 0;
+// while (number <= 50)
+// {
+//     console.log(number);
+//     number++;
+// }
+
+while (number<=20){
+    console.log(number);
+    // number++;
+    number = number + 2;
+}
--- while.js
+++ while.js
@@ -1,5 +1,8 @@
 var roastGiven = 0;
-
+// 1. loop variable
+// 2. condition
+// 3. loop body
+// 4. change the loop variable
 while(roastGiven < 7)
 {
     console.log('roast please.');
