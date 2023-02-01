0755f613846ca1a0f29c3692004e39874f05db6b
farhanthegreat <farhantuber@gmail.com>
2023-02-01 20:59:55 +0600

    array loop added

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+var numbers = [45,44,34,5,57,98,9,45,33];
+// for(var i=0; i<numbers.length; i++)
+// {
+//     var number = numbers[i];
+//     console.log(number);
+// }
+var itemsTable = ['toothpaste', 'pen', 'laptop', 'notebook', 'mobile'];
+for(var i =0; i < itemsTable.length; i++)
+{
+    var item = itemsTable[i];
+    console.log(item);
+}
