ae25f619b212b8615edbf02ed867dcdcbbef21b6
farhanthegreat <farhantuber@gmail.com>
2023-02-01 18:35:52 +0600

    revision done

--- revision.js
+++ revision.js
@@ -1,2 +1,18 @@
 var bottle = 'water';
 var bottleColor = 'yellow';
+var tableItems = ['watch', 'books', 'mobile', 'laptop'];
+var item3 = tableItems[2];
+tableItems[3] = 'notebook';
+var bookIndex = tableItems.indexOf('books');
+if (tableItems.length > 4)
+{
+    console.log (tableItems);
+
+}
+else if( tableItems[3] == 'notebook')
+{
+    console.log('amar new notebook.')
+}
+else{
+    console.log('amar pocket faka.')
+}
